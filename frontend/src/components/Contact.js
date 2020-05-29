02305f13317269e542a7a76a244fbbf0fee0c679
Donald Yeh <Donald.Yeh@gmail.com>
2020-05-29 13:13:25 -0400

    grids and centers the social media links paper

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -45,6 +45,9 @@ const useStyles = makeStyles({
 		padding: "1.75rem",
 		margin: "1rem",
 	},
+	mainPaper: {
+		padding: "1.25rem",
+	},
 	hasError: {
 		border: "1px solid #f44336",
 		background: "#f44336",
@@ -116,213 +119,234 @@ const Contact = () => {
 		<>
 			<Navbar />
 			{/* <Box> */}
-			<Grid
-				container
-				// alignItems="center"
-				spacing={3}
-				className={classes.gridContainer}
-			>
-				<Grid item xs={12} className={classes.gridItem}>
-					<Paper className={classes.paper}>
-						<Typography>Let's build together</Typography>
-						<Typography>
-							Have a project you're looking to get off the ground?
-							I'd love to hear from you. Send a message to get the
-							ball rolling.
-						</Typography>
-					</Paper>
-				</Grid>
+			<Paper className={classes.mainPaper}>
 				<Grid
-					item
 					container
-					xs={12}
 					justify="center"
-					className={classes.gridItem}
+					alignItems="center"
+					spacing={3}
+					className={classes.gridContainer}
 				>
-					<Grid item md={6}>
+					<Grid item xs={12} className={classes.gridItem}>
 						<Paper className={classes.paper}>
-							<Formik
-								initialValues={{
-									firstName: "",
-									lastName: "",
-									email: "",
-									message: "",
-								}}
-								onSubmit={handleSubmit}
-								validationSchema={validationSchema}
-							>
-								{({
-									values,
-									errors,
-									touched,
-									handleChange,
-									handleBlur,
-								}) => (
-									<Form>
-										<div>
-											<Field
-												name="firstName"
-												component={MyField}
-												label="first name"
-											>
-												<TextField
+							<Typography>Let's build together</Typography>
+							<Typography>
+								Have a project you're looking to get off the
+								ground? I'd love to hear from you. Send a
+								message to get the ball rolling.
+							</Typography>
+						</Paper>
+					</Grid>
+					<Grid
+						item
+						container
+						xs={12}
+						justify="center"
+						className={classes.gridItem}
+					>
+						<Grid item md={6}>
+							<Paper className={classes.paper}>
+								<Formik
+									initialValues={{
+										firstName: "",
+										lastName: "",
+										email: "",
+										message: "",
+									}}
+									onSubmit={handleSubmit}
+									validationSchema={validationSchema}
+								>
+									{({
+										values,
+										errors,
+										touched,
+										handleChange,
+										handleBlur,
+									}) => (
+										<Form>
+											<div>
+												<Field
 													name="firstName"
-													value={values.firstName}
-													onChange={handleChange}
-													onBlur={handleBlur}
+													component={MyField}
+													label="first name"
+												>
+													<TextField
+														name="firstName"
+														value={values.firstName}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														// className={
+														// 	touched.firstName &&
+														// 	errors.firstName
+														// 		? "classes.hasError"
+														// 		: null
+														// }
+														variant="filled"
+													/>
+												</Field>
+												<Error
+													touched={touched.firstName}
+													message={errors.firstName}
+													className={classes.invalid}
 													// className={
 													// 	touched.firstName &&
 													// 	errors.firstName
 													// 		? "classes.hasError"
 													// 		: null
 													// }
-													variant="filled"
 												/>
-											</Field>
-											<Error
-												touched={touched.firstName}
-												message={errors.firstName}
-												className={classes.invalid}
-												// className={
-												// 	touched.firstName &&
-												// 	errors.firstName
-												// 		? "classes.hasError"
-												// 		: null
-												// }
-											/>
-										</div>
+											</div>
 
-										<div>
-											<Field
-												name="lastName"
-												component={MyField}
-												label="last name"
-											>
-												<TextField
+											<div>
+												<Field
 													name="lastName"
-													value={values.lastName}
-													onChange={handleChange}
-													onBlur={handleBlur}
+													component={MyField}
+													label="last name"
+												>
+													<TextField
+														name="lastName"
+														value={values.lastName}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														className={
+															touched.lastName &&
+															errors.lastName
+																? "classes.hasError"
+																: null
+														}
+														variant="filled"
+													/>
+												</Field>
+												<Error
+													touched={touched.lastName}
+													message={errors.lastName}
 													className={
 														touched.lastName &&
 														errors.lastName
 															? "classes.hasError"
 															: null
 													}
-													variant="filled"
 												/>
-											</Field>
-											<Error
-												touched={touched.lastName}
-												message={errors.lastName}
-												className={
-													touched.lastName &&
-													errors.lastName
-														? "classes.hasError"
-														: null
-												}
-											/>
-										</div>
+											</div>
 
-										<div>
-											<Field
-												name="email"
-												component={MyField}
-												label="email"
+											<div>
+												<Field
+													name="email"
+													component={MyField}
+													label="email"
+													className={
+														touched.email &&
+														errors.email
+															? "classes.hasError"
+															: null
+													}
+												>
+													<TextField
+														name="email"
+														value={values.email}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														variant="outlined"
+													/>
+												</Field>
+												<Error
+													touched={touched.email}
+													message={errors.email}
+												/>
+											</div>
+											<div
 												className={
-													touched.email &&
-													errors.email
+													touched.message &&
+													errors.message
 														? "classes.hasError"
 														: null
 												}
 											>
-												<TextField
-													name="email"
-													value={values.email}
-													onChange={handleChange}
-													onBlur={handleBlur}
-													variant="outlined"
+												<Field
+													name="message"
+													component={MyField}
+													label="message"
+												>
+													<TextField
+														name="message here"
+														value={values.message}
+														onChange={handleChange}
+														onBlur={handleBlur}
+														variant="outlined"
+														helperText={
+															errors.message
+														}
+													/>
+												</Field>
+												<Error
+													touched={touched.message}
+													message={errors.message}
 												/>
-											</Field>
-											<Error
-												touched={touched.email}
-												message={errors.email}
-											/>
-										</div>
-										<div
-											className={
-												touched.message &&
-												errors.message
-													? "classes.hasError"
-													: null
-											}
-										>
-											<Field
-												name="message"
-												component={MyField}
-												label="message"
-											>
-												<TextField
-													name="message here"
-													value={values.message}
-													onChange={handleChange}
-													onBlur={handleBlur}
-													variant="outlined"
-													helperText={errors.message}
-												/>
-											</Field>
-											<Error
-												touched={touched.message}
-												message={errors.message}
-											/>
-										</div>
-										<Button type="submit">submit</Button>
-										{/* <pre>
+											</div>
+											<Button type="submit">
+												submit
+											</Button>
+											{/* <pre>
 												{JSON.stringify(
 													values,
 													null,
 													2
 												)}
 											</pre> */}
-									</Form>
-								)}
-							</Formik>
+										</Form>
+									)}
+								</Formik>
+							</Paper>
+						</Grid>
+					</Grid>
+					<Grid
+						item
+						container
+						xs={12}
+						md={6}
+						className={classes.gridItem}
+						justify="center"
+					>
+						<Paper className={classes.paper}>
+							<Typography>Let's Connect</Typography>
+							<Grid container justify="space-evenly">
+								<Grid item>
+									<Link
+										href="https://www.linkedin.com/in/donald-yeh-b3b1426/"
+										// onClick={preventDefault}
+										variant="body2"
+									>
+										{/* {'variant="body2"'} */}
+										<LinkedInIcon />
+									</Link>
+								</Grid>
+
+								<Grid item>
+									<Link
+										href="https://github.com/DonYeh"
+										// onClick={preventDefault}
+										variant="body2"
+									>
+										{/* {'variant="body2"'} */}
+										<GitHubIcon />
+									</Link>
+								</Grid>
+
+								<Grid item>
+									<Link
+										href="https://twitter.com/heydonaldyeh"
+										// onClick={preventDefault}
+										variant="body2"
+									>
+										{/* {'variant="body2"'} */}
+										<TwitterIcon />
+									</Link>
+								</Grid>
+							</Grid>
 						</Paper>
 					</Grid>
 				</Grid>
-				<Grid item xs={12} className={classes.gridItem}>
-					<Paper className={classes.paper}>
-						<Typography>Let's Connect</Typography>
-						<Typography>
-							Social media links here
-							<Link
-								href="https://www.linkedin.com/in/donald-yeh-b3b1426/"
-								// onClick={preventDefault}
-								variant="body2"
-							>
-								{/* {'variant="body2"'} */}
-								<LinkedInIcon />
-							</Link>
-							<Link
-								href="https://github.com/DonYeh"
-								// onClick={preventDefault}
-								variant="body2"
-							>
-								{/* {'variant="body2"'} */}
-								<GitHubIcon />
-							</Link>
-							<Link
-								href="https://twitter.com/heydonaldyeh"
-								// onClick={preventDefault}
-								variant="body2"
-							>
-								{/* {'variant="body2"'} */}
-								<TwitterIcon />
-							</Link>
-						</Typography>
-					</Paper>
-				</Grid>
-			</Grid>
+			</Paper>
 			{/* </Box> */}
 		</>
 	);
