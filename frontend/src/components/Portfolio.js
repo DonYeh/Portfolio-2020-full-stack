eb2ec907b368580d4a6b8a0fce83cc82c1ce2a95
Donald Yeh <Donald.Yeh@gmail.com>
2020-05-30 00:08:52 -0400

    adds responsiveness to project cards in  Portfolio component

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -15,10 +15,16 @@ const useStyles = makeStyles({
 		maxWidth: 350,
 		// background: "white",
 	},
+	mainBox: {
+		margin: "2rem",
+	},
 	mainGridContainer: {
-		display: "flex",
+		// display: "flex",
 		// background: "black",
-		alignItems: "center",
+		margin: "3rem",
+		padding: "2em",
+		height: "50%",
+		// background: "pink",
 	},
 	proj1: {},
 });
@@ -29,145 +35,157 @@ export default function ImgMediaCard() {
 	return (
 		<>
 			<Navbar />
-			<Grid
-				className="mainGridContainer"
-				container
-				spacing={3}
-				justify="center"
-				alignContent="center"
-			>
-				<Grid item>
-					<Box className="proj1">
-						<Card className={classes.root}>
-							<CardActionArea>
-								<CardMedia
-									component="img"
-									alt="Contemplative Reptile"
-									height="140"
-									image="/static/images/cards/contemplative-reptile.jpg"
-									title="Contemplative Reptile"
-								/>
-								<CardContent>
-									<Typography
-										gutterBottom
-										variant="h5"
-										component="h2"
-									>
-										donaldyeh.dev
-									</Typography>
-									<Typography
-										variant="body2"
-										color="textSecondary"
-										component="p"
-									>
-										This portfolio site is written in React,
-										Typescript,and styled with Material-UI
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-							<CardActions>
-								<Button size="small" color="primary">
-									Share
-								</Button>
-								<Button size="small" color="primary">
-									Learn More
-								</Button>
-							</CardActions>
-						</Card>
-					</Box>
-				</Grid>
+			<Box className="classes.mainBox">
+				<Grid
+					className="classes.mainGridContainer"
+					container
+					spacing={4}
+					justify="space-evenly"
+					alignContent="center"
+					style={{ padding: "11%" }}
+				>
+					<Grid item>
+						<Box className="proj1">
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="Contemplative Reptile"
+										height="140"
+										image="/static/images/cards/contemplative-reptile.jpg"
+										title="Contemplative Reptile"
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+										>
+											donaldyeh.dev
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+										>
+											This portfolio site is a full stack
+											Javascript app. The front end is
+											written in React and leverages
+											Material-UI responsive design. The
+											back end consists of Node, Express,
+											and Nodemailer, which is used to
+											send an email from the contact form.
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button size="small" color="primary">
+										Share
+									</Button>
+									<Button size="small" color="primary">
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Box>
+					</Grid>
 
-				<Grid item>
-					<Box className="proj2">
-						<Card className={classes.root}>
-							<CardActionArea>
-								<CardMedia
-									component="img"
-									alt="Contemplative Reptile"
-									height="140"
-									image="/static/images/cards/contemplative-reptile.jpg"
-									title="Contemplative Reptile"
-								/>
-								<CardContent>
-									<Typography
-										gutterBottom
-										variant="h5"
-										component="h2"
-									>
-										Chatris
-									</Typography>
-									<Typography
-										variant="body2"
-										color="textSecondary"
-										component="p"
-									>
-										Chatris is a cross-platform, full-stack
-										mobile app that allows you to enjoy a
-										timeless classic while connecting with
-										your friends over video and voice chat.
-										Chatris is built using React Native,
-										PostgreSQL, Node, Express, and Socket.io
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-							<CardActions>
-								<Button size="small" color="primary">
-									Share
-								</Button>
-								<Button size="small" color="primary">
-									Learn More
-								</Button>
-							</CardActions>
-						</Card>
-					</Box>
+					<Grid item>
+						<Box className="proj2">
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="Contemplative Reptile"
+										height="140"
+										image="/static/images/cards/contemplative-reptile.jpg"
+										title="Contemplative Reptile"
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+										>
+											Chatris
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+										>
+											Chatris is a cross-platform,
+											full-stack mobile app that allows
+											you to enjoy a puzzle game classic
+											while connecting with your friends
+											over live video and voice chat.
+											Chatris is built using React Native,
+											PostgreSQL, Node, Express, and
+											Socket.io
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button size="small" color="primary">
+										Share
+									</Button>
+									<Button size="small" color="primary">
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Box>
+					</Grid>
+
+					<Grid item>
+						<Box className="proj3">
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="Contemplative Reptile"
+										height="140"
+										image="/static/images/cards/contemplative-reptile.jpg"
+										title="Contemplative Reptile"
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+										>
+											Movie Explorer
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+										>
+											Movie Explorer allows you to browse
+											and discover movies. It uses a
+											responsive, mobile-first design
+											featuring advanced filtering and
+											search functionality. It is built
+											with Javascript, HTML, CSS and it
+											calls the OMDB and TMDB APIs for
+											up-to-date movie information and
+											trailers.
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button size="small" color="primary">
+										Share
+									</Button>
+									<Button size="small" color="primary">
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Box>
+					</Grid>
 				</Grid>
-				<Grid item>
-					<Box className="proj3">
-						<Card className={classes.root}>
-							<CardActionArea>
-								<CardMedia
-									component="img"
-									alt="Contemplative Reptile"
-									height="140"
-									image="/static/images/cards/contemplative-reptile.jpg"
-									title="Contemplative Reptile"
-								/>
-								<CardContent>
-									<Typography
-										gutterBottom
-										variant="h5"
-										component="h2"
-									>
-										Movie Explorer
-									</Typography>
-									<Typography
-										variant="body2"
-										color="textSecondary"
-										component="p"
-									>
-										Movie Explorer allows you to browse and
-										discover movies. It uses a responsive,
-										mobile-first design featuring advanced
-										filtering and search functionality. It
-										is built with Javascript, HTML, CSS and
-										it calls the OMDB and TMDB APIs for
-										up-to-date movie information and
-										trailers.
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-							<CardActions>
-								<Button size="small" color="primary">
-									Share
-								</Button>
-								<Button size="small" color="primary">
-									Learn More
-								</Button>
-							</CardActions>
-						</Card>
-					</Box>
-				</Grid>
-			</Grid>
+			</Box>
 		</>
 	);
 }
--- frontend/src/components/Portfolio.tsx
+++ frontend/src/components/Portfolio.tsx
@@ -15,6 +15,9 @@ const useStyles = makeStyles({
 		maxWidth: 350,
 		// background: "white",
 	},
+	mainBox: {
+		margin: "2rem",
+	},
 	mainGridContainer: {
 		display: "flex",
 		// background: "black",
@@ -29,145 +32,151 @@ export default function ImgMediaCard() {
 	return (
 		<>
 			<Navbar />
-			<Grid
-				className="mainGridContainer"
-				container
-				spacing={3}
-				justify="center"
-				alignContent="center"
-			>
-				<Grid item>
-					<Box className="proj1">
-						<Card className={classes.root}>
-							<CardActionArea>
-								<CardMedia
-									component="img"
-									alt="Contemplative Reptile"
-									height="140"
-									image="/static/images/cards/contemplative-reptile.jpg"
-									title="Contemplative Reptile"
-								/>
-								<CardContent>
-									<Typography
-										gutterBottom
-										variant="h5"
-										component="h2"
-									>
-										donaldyeh.dev
-									</Typography>
-									<Typography
-										variant="body2"
-										color="textSecondary"
-										component="p"
-									>
-										This portfolio site is written in React,
-										Typescript,and styled with Material-UI
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-							<CardActions>
-								<Button size="small" color="primary">
-									Share
-								</Button>
-								<Button size="small" color="primary">
-									Learn More
-								</Button>
-							</CardActions>
-						</Card>
-					</Box>
-				</Grid>
+			<Box className={classes.mainBox}>
+				<Grid
+					className="mainGridContainer"
+					container
+					spacing={3}
+					justify="center"
+					alignContent="center"
+				>
+					<Grid item>
+						<Box className="proj1">
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="Contemplative Reptile"
+										height="140"
+										image="/static/images/cards/contemplative-reptile.jpg"
+										title="Contemplative Reptile"
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+										>
+											donaldyeh.dev
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+										>
+											This portfolio site is written in
+											React, Typescript,and styled with
+											Material-UI
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button size="small" color="primary">
+										Share
+									</Button>
+									<Button size="small" color="primary">
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Box>
+					</Grid>
 
-				<Grid item>
-					<Box className="proj2">
-						<Card className={classes.root}>
-							<CardActionArea>
-								<CardMedia
-									component="img"
-									alt="Contemplative Reptile"
-									height="140"
-									image="/static/images/cards/contemplative-reptile.jpg"
-									title="Contemplative Reptile"
-								/>
-								<CardContent>
-									<Typography
-										gutterBottom
-										variant="h5"
-										component="h2"
-									>
-										Chatris
-									</Typography>
-									<Typography
-										variant="body2"
-										color="textSecondary"
-										component="p"
-									>
-										Chatris is a cross-platform, full-stack
-										mobile app that allows you to enjoy a
-										timeless classic while connecting with
-										your friends over video and voice chat.
-										Chatris is built using React Native,
-										PostgreSQL, Node, Express, and Socket.io
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-							<CardActions>
-								<Button size="small" color="primary">
-									Share
-								</Button>
-								<Button size="small" color="primary">
-									Learn More
-								</Button>
-							</CardActions>
-						</Card>
-					</Box>
+					<Grid item>
+						<Box className="proj2">
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="Contemplative Reptile"
+										height="140"
+										image="/static/images/cards/contemplative-reptile.jpg"
+										title="Contemplative Reptile"
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+										>
+											Chatris
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+										>
+											Chatris is a cross-platform,
+											full-stack mobile app that allows
+											you to enjoy a timeless classic
+											while connecting with your friends
+											over video and voice chat. Chatris
+											is built using React Native,
+											PostgreSQL, Node, Express, and
+											Socket.io
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button size="small" color="primary">
+										Share
+									</Button>
+									<Button size="small" color="primary">
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Box>
+					</Grid>
+					<Grid item>
+						<Box className="proj3">
+							<Card className={classes.root}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										alt="Contemplative Reptile"
+										height="140"
+										image="/static/images/cards/contemplative-reptile.jpg"
+										title="Contemplative Reptile"
+									/>
+									<CardContent>
+										<Typography
+											gutterBottom
+											variant="h5"
+											component="h2"
+										>
+											Movie Explorer
+										</Typography>
+										<Typography
+											variant="body2"
+											color="textSecondary"
+											component="p"
+										>
+											Movie Explorer allows you to browse
+											and discover movies. It uses a
+											responsive, mobile-first design
+											featuring advanced filtering and
+											search functionality. It is built
+											with Javascript, HTML, CSS and it
+											calls the OMDB and TMDB APIs for
+											up-to-date movie information and
+											trailers.
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions>
+									<Button size="small" color="primary">
+										Share
+									</Button>
+									<Button size="small" color="primary">
+										Learn More
+									</Button>
+								</CardActions>
+							</Card>
+						</Box>
+					</Grid>
 				</Grid>
-				<Grid item>
-					<Box className="proj3">
-						<Card className={classes.root}>
-							<CardActionArea>
-								<CardMedia
-									component="img"
-									alt="Contemplative Reptile"
-									height="140"
-									image="/static/images/cards/contemplative-reptile.jpg"
-									title="Contemplative Reptile"
-								/>
-								<CardContent>
-									<Typography
-										gutterBottom
-										variant="h5"
-										component="h2"
-									>
-										Movie Explorer
-									</Typography>
-									<Typography
-										variant="body2"
-										color="textSecondary"
-										component="p"
-									>
-										Movie Explorer allows you to browse and
-										discover movies. It uses a responsive,
-										mobile-first design featuring advanced
-										filtering and search functionality. It
-										is built with Javascript, HTML, CSS and
-										it calls the OMDB and TMDB APIs for
-										up-to-date movie information and
-										trailers.
-									</Typography>
-								</CardContent>
-							</CardActionArea>
-							<CardActions>
-								<Button size="small" color="primary">
-									Share
-								</Button>
-								<Button size="small" color="primary">
-									Learn More
-								</Button>
-							</CardActions>
-						</Card>
-					</Box>
-				</Grid>
-			</Grid>
+			</Box>
 		</>
 	);
 }
