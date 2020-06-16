44955e6339f0bd0a8d25d46109566d9e85532165
Donald Yeh <Donald.Yeh@gmail.com>
2020-06-16 17:19:28 -0400

    removes unneccesary imports in `portfolio`

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -11,8 +11,6 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 
-// import Navbar from "./Navbar";
-// import Navbar3 from "./Navbar3";
 import {
 	Typography,
 	Box,
